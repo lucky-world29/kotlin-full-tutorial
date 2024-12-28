df7017d114e8e1ba21a380a84b2b39a2c8f0729d
ABINASH BEHERA <128899808+lucky-1-4-3@users.noreply.github.com>
2024-12-28 15:58:34 +0530

    hello world and something about the variables

---
+++ basic-kotlin/basic/src/Main.kt
@@ -0,0 +1,35 @@
+//fun main() {
+//    println("Hello World")
+//}
+
+//Main Parameters
+
+
+//fun main(args : Array<String>) {
+//    println("Hello World")
+//}
+
+//
+
+//fun main() {
+//    println("Hello World!")
+//    println("I am learning Kotlin.")
+//    println("It is awesome!")
+//}
+fun main() {
+    println(3 + 3)
+}
+
+//var name = "John"
+//val birthyear = 1975
+//
+//println(name)          // Print the value of name
+//println(birthyear)     // Print the value of birthyear
+
+//fun main() {
+//    var name = "Lucky"
+//    val birthyear = 2001
+//
+//    println(name)
+//    println(birthyear)
+//}
