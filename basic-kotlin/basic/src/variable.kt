8414f66eddeaaa6f3d2864f0f992a4e7a814fc45
ABINASH BEHERA <128899808+lucky-1-4-3@users.noreply.github.com>
2024-12-30 00:58:04 +0530

    a little modification nothing important ..

--- basic-kotlin/basic/src/variable.kt
+++ basic-kotlin/basic/src/variable.kt
@@ -13,4 +13,16 @@ fun main() {
     val lastName = "Mate"
     val fullName = firstName + lastName
     println(fullName)
+
+    // Prints a message to request input
+    println("Enter any word: ")
+
+// Reads and stores the user input. For example: Happiness
+    val yourWord = readln()
+
+
+// Prints a message with the input
+    print("You entered the word: ")
+    print(yourWord)
+// You entered the word: Happiness
 }
