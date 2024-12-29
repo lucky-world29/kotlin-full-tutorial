f7912bfaa9a01f2f9141982ab1cf75ff714421a0
ABINASH BEHERA <128899808+lucky-1-4-3@users.noreply.github.com>
2024-12-29 08:05:28 +0530

    variable.kt created

---
+++ basic-kotlin/basic/src/variable.kt
@@ -0,0 +1,15 @@
+//fun main() {
+//    var name = "Lucky" // String
+//    val birthyear = 2001 // Int
+//
+//    println(name)
+//    println(birthyear)
+//}
+//
+
+fun main() {
+    val firstName = "Soul "
+    val lastName = "Mate"
+    val fullName = firstName + lastName
+    println(fullName)
+}
