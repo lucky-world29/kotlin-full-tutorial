8a576034c5fe0b6f27bfcada5000a623fbe8ca1b
ABINASH BEHERA <128899808+lucky-1-4-3@users.noreply.github.com>
2024-12-30 22:31:54 +0530

    Data types added

---
+++ basic-kotlin/basic/src/Datatypes.kt
@@ -0,0 +1,26 @@
+// fun main() {
+//     val myNum = 5               // Int
+//     val myDoubleNum = 5.99      // Double
+//     val myLetter = 'D'          // Char
+//     val myBoolean = true        // Boolean
+//     val myText = "Hello"        // String
+//     println(myNum)
+//     println(myDoubleNum)
+//     println(myLetter)
+//     println(myBoolean)
+//     println(myText)
+//   }
+
+
+fun main() {
+    val myNum: Int = 5                // Int
+    val myDoubleNum: Double = 5.99    // Double
+    val myLetter: Char = 'D'          // Char
+    val myBoolean: Boolean = true     // Boolean
+    val myText: String = "Hello"      // String
+    println(myNum)
+    println(myDoubleNum)
+    println(myLetter)
+    println(myBoolean)
+    println(myText)
+  }
--- basic-kotlin/basic/src/Main.kt
+++ basic-kotlin/basic/src/Main.kt
@@ -33,3 +33,4 @@ fun main() {
 //    println(name)
 //    println(birthyear)
 //}
+
